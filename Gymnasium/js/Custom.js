087f9d5bad5fcf9aac2058813f5d15203fd743ee
Shakaut Kader <shakautkader608@gmail.com>
2021-11-16 14:47:19 +0600

    Design psd to html

---
+++ Gymnasium/js/Custom.js
@@ -0,0 +1,63 @@
+$('.banner_slide').slick({
+    dots: true,
+    speed: 900,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+});
+//Banner part ends  here
+
+$('.venobox').venobox(); 
+//Gallery end
+
+$('.team_slide').slick({
+    dots: false,
+    speed: 900,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+});
+//Our team part end
+
+$('.test_slide').slick({
+    dots: true,
+    speed: 900,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+});
+//Testimonial Part ends here
+
+$('.counter').counterUp({
+     delay: 10,
+     time: 1000
+});
+
+//counterpart ends
+
+$('.partner_slide').slick({
+    dots: true,
+    speed: 900,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
